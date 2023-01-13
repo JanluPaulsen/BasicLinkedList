0011ad02c3e9bfc94dd272b974210b4b92eb0e1d
JanluPaulsen <jpaulsen1317@gmail.com>
2023-01-13 13:36:33 +0200

    Basic Linked List

---
+++ src/BasicLinkedList.java
@@ -0,0 +1,11 @@
+public class BasicLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList aList = new MyLinkedList();
+        aList.append(5);
+        aList.append(6);
+        aList.append(7);
+        aList.append(8);
+
+        aList.display();
+    }
+}
